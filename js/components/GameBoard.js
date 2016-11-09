731dc1d5d92756523a3b74022057b6371066f9a5
Benjamin Soung <soungbenjamin@gmail.com>
2016-11-09 15:34:38 -0500

    add more functionality

--- js/components/GameBoard.js
+++ js/components/GameBoard.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import actions from '../actions/actions'
 import { connect } from 'react-redux'
-let numbers = ''
 
-//{this.props.guesses.length === 0 ? this.props.defaultGuess : this.props.guesses.join(' ')}
+const MAX = 11
 const GameBoard = React.createClass({
 	checkNumber: function() {
 		let numberGuess=this.refs.numberGuess.value
-		// 
+		
 		this.props.dispatch(actions.checkNumber(numberGuess));
 	},
 
@@ -16,14 +15,8 @@ const GameBoard = React.createClass({
 	},
 
 	render: function() {
-
-		let test = [{result: 'blah'}]
-		// console.log('THIS IS OUR PROPS', this.props)
+		console.log('THIS IS OUR PROPS', this.props)
 		
-		numbers += (' ' + this.props.guesses)
-		console.log(numbers)
-		
-		// console.log(this.props.guesses)
 		return (
 			<div className='container'>
 
@@ -32,24 +25,24 @@ const GameBoard = React.createClass({
 				</div>
 
 				<div className='guesses'>
-					Your Guesses: {numbers}
+					Your Guesses: {this.props.numbers.join(' ')}
 				</div>
 
 				<div className='input'>
-					<input type='number' ref='numberGuess' placeholder='Enter a Number!' />
+					{this.props.numbers.length < MAX ? <input type='number' ref='numberGuess' placeholder='Enter a Number!' /> : 'YOU LOST!'}
 				</div>
 
 				<div className='buttons'>
-					<button type='button' onClick={this.checkNumber}>
+					{this.props.numbers.length < MAX ? <button type='button' onClick={this.checkNumber}>
 					Make a Guess!
-					</button>
+					</button> : ''}
 					<button type='button' onClick={this.restartGame}>
 					Restart
 					</button>
 				</div>
 
 				<div className='result'>
-					{this.props.result}
+					{this.props.numbers.length < MAX ? this.props.result : 'Please try again!'}
 				</div>
 
 			</div>
@@ -60,7 +53,7 @@ const GameBoard = React.createClass({
 
 const mapStateToProps = function(state, props) {
 	return {
-		guesses: state.guesses,
+		numbers: state.numbers,
 		defaultGuess: state.guess,
 		result: state.result
 	}
--- js/reducers/reducers.js
+++ js/reducers/reducers.js
@@ -2,40 +2,43 @@ import actions from '../actions/actions'
 import _ from 'lodash'
 
 const reducer = (state={}, action={}) => {
-	let payload = {guesses: ''}
+	let payload = {}
 	
-
 	switch (action.type) {
 		case actions.CHECK_NUMBER:
+			if (state.numbers.indexOf(action.number) !== -1) {
+				payload.result = 'You already entered this number!'
+				return Object.assign({}, state, payload)
+			}
+
 			if (state.randomNumber - action.number === 0) {
-				payload['result'] = 'WINNER WINNER CHICKEN DINNER!'
+				payload.result = 'WINNER WINNER CHICKEN DINNER!'
 				
 			} else if (Math.abs(state.randomNumber - action.number) <= 2) {
-				payload['result'] = 'Call the fire department because you\'re on fire!'
+				payload.result = 'Call the fire department because you\'re on fire!'
 				
 			} else if (Math.abs(state.randomNumber - action.number) <= 15) {
-				payload['result'] = 'You\'re getting hotter!'
+				payload.result = 'You\'re getting hotter!'
 				
 			} else if (Math.abs(state.randomNumber - action.number) <= 30) {
-				payload['result'] = 'You\'re reaching room temperature!'
+				payload.result = 'You\'re reaching room temperature!'
 				
 			} else if (Math.abs(state.randomNumber - action.number) <= 60) {
-				payload['result'] = 'Grab a jacket because you\'re getting cold!'
+				payload.result = 'Grab a jacket because you\'re getting cold!'
 				
 			} else {
-				payload['result'] = 'Did you move to Antarctica- brrrrrr!'
+				payload.result = 'Did you move to Antarctica- brrrrrr!'
 				
 			} 
-			
-			payload.guesses += action.number
-			
+			payload.numbers = state.numbers.concat(action.number)
+	
 			return Object.assign({}, state, payload);
 
 
 		case actions.RESTART_GAME:
 			let randomNumber = Math.floor(Math.random() * 100) + 1
 			console.log(randomNumber)
-			return Object.assign({}, state, {result: 'Are you ready for infinite fun?!', randomNumber, guess: 'Your Guesses: ', guesses: ''});
+			return Object.assign({}, state, {result: 'Are you ready for infinite fun?!', randomNumber, guess: 'Your Guesses: ', numbers: []});
 
 		default:
 
